16e80a1151ef36b1e1b5534c07232839b8a006c6
Piotr Jodko <piotr.jodko@gmail.com>
2019-05-27 09:41:14 +0200

    resent changes

--- RESTapi/docker-REST/Lesson4/models.py
+++ RESTapi/docker-REST/Lesson4/models.py
@@ -3,10 +3,24 @@ from sqlalchemy import Column,Integer,String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
-from passlib.apps import custom_app_context as pwd_context
+#from passlib.apps import custom_app_context as pwd_context
 Base = declarative_base()
 
 #ADD YOUR USER MODEL HERE
+class User(Base):
+	__tablename__ = 'user'
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String)
+	passwordHash = Column(String)
+
+	@property
+	def serialize(self):
+		return {
+			'name': self.name,
+			'passwordHash': self.passwordHash
+		}
+
 
 class Bagel(Base):
 	__tablename__ = 'bagel'
