f76424bc3633625dfd687b3805cce30162f82fec
Piotr Jodko <piotr.jodko@gmail.com>
2019-05-27 09:17:06 +0200

    fix for httpauth

--- RESTapi/docker-REST/Lesson4/views.py
+++ RESTapi/docker-REST/Lesson4/views.py
@@ -4,7 +4,8 @@ from flask import Flask, jsonify, request, url_for, abort, g
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
-from flask.ext.httpauth import HTTPBasicAuth
+
+from flask_httpauth import HTTPBasicAuth
 
 auth = HTTPBasicAuth() 
 
